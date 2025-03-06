fc534e885b1b76e5baeea4af495d78db045ab999
Tejas Raikwar <89740546+TejasRaikwar@users.noreply.github.com>
2025-03-06 15:46:49 +0530

    Add files via upload

---
+++ day6/employeeClass.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+
+class employee{
+	private:
+		string empNo;
+		string name;
+		double salary;
+		string dept;
+
+	void Accept(){
+		cout<<"please enter empNo:->";
+		cin>>empNo;
+		cout<<"please enter employee name:->";
+		cin>>name;
+		cout<<"please enter epmployee salary:->";
+		cin>>salary;
+		cout<<"please emter epmloyee dept:->";
+		cin>>dept;
+
+	}
+	void Display(){
+	
+	}
+
+
+};
+
+int main(){
+	
+	return 0;
+}
