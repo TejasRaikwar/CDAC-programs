573c7770db20e97c9e0bccd4b9ab6f200d763641
Tejas Raikwar <raikwar.tejas@gmail.com>
2025-04-09 15:22:10 +0530

    Update secondLargestInArray.java

--- TejasWorkplace/DayWisePrograms/src/com/geeksforgeeks/DSA/Arrays/secondLargestInArray.java
+++ TejasWorkplace/DayWisePrograms/src/com/geeksforgeeks/DSA/Arrays/secondLargestInArray.java
@@ -1,9 +1,9 @@
 package com.geeksforgeeks.DSA.Arrays;
 
 public class secondLargestInArray {
-	int getLargest(int[] arr, int n) {
+	int getLargest(int[] arr) {
 		int largest = 0;
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < arr.length; i++) {
 			if (arr[i] > arr[largest]) {
 				largest = i;
 			}
@@ -11,17 +11,17 @@ public class secondLargestInArray {
 		return largest;
 	}
 
-	int secondLargest(int[] arr, int n) {
-		int largest = getLargest(arr, arr.length);
+	int secondLargest(int[] arr) {
+		int largest = getLargest(arr);
 		int result = -1;
-		for (int i = 0; i < n; i++) {
-            if(arr[i] != arr[largest]) {
-            	if(result == -1)
-            		result = i;
-            	else if(arr[i] > arr[result]) {
-            		result = i;
-            	}
-            }
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] != arr[largest]) {
+				if (result == -1)
+					result = i;
+				else if (arr[i] > arr[result]) {
+					result = i;
+				}
+			}
 		}
 		return result;
 	}
@@ -29,8 +29,7 @@ public class secondLargestInArray {
 	public static void main(String[] args) {
 		secondLargestInArray s = new secondLargestInArray();
 		int arr[] = new int[] { 1, 122, 3, 4, 3, 2, 12, 32, 4, 43, 66, 99 };
-		// int index = s.getLargest(arr, arr.length);
-		System.out.println(arr[s.secondLargest(arr, arr.length)]);
+		System.out.println(arr[s.secondLargest(arr)]);
 
 	}
 
