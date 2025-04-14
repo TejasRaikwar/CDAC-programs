5572041c62cb05ce09519a3983f5b1ba4ccce317
Tejas Raikwar <raikwar.tejas@gmail.com>
2025-04-14 15:22:01 +0530

    some mess

--- TejasWorkplace/DayWisePrograms/src/devJava/Main.java
+++ TejasWorkplace/DayWisePrograms/src/devJava/Main.java
@@ -1,13 +1,18 @@
 package devJava;
 
 
-public class Main {
-	public static void main(String[] args) {
-		nestedClasses nc = new nestedClasses();
-		nestedClasses.innerClass ni = new nestedClasses().new innerClass();
-		ni.greet();
-		new nestedClasses().new innerClass().greet();
-	}
+class Main {
+//	public static void main(String[] args) {
+//		nestedClasses nc = new nestedClasses();
+//		nestedClasses.innerClass ni = new nestedClasses().new innerClass();
+//		ni.greet();
+//		new nestedClasses().new innerClass().greet();
+//	}
+	
 }
 
-//
+
+record names(int name, int roll) {
+	
+	
+}
